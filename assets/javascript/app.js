31406d26cda415c3be4151fbf7c138d229846c33
mtvalencia <mtv411@yahoo.com>
2019-04-09 02:25:46 -0600

    Draft code

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+//global variables
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+var gameStatus = "Start";
+
+//Questions
+var q1 = {
+    question:"Amityville is a city in Indiana.",
+    answer:false,
+    noAnswer:true};
+    document.getElementById("q1").innerHTML = q1.question;
+    var answer1 = a1.value;
+    function pickA1() {
+        console.log(answer1);
+    }
+
+var q2 = {
+    question:"Parts of the Lost Boys movie was filmed in Santa Cruz, California.",
+    answer:true,
+    noAnswer:true};
+
+    
+var q3 = {
+    question:"Jeffrey Dahmer was a vegetarian.",
+    answer:false,
+    noAnswer:true};
+    
+var q4 = {
+    question:"Ted Bundy escaped from a jail in Colorado.",
+    answer:true,
+    noAnswer:true};
+
+var q5 = {
+    question:"Wes Craven directed the movie Poltergeist.",
+    answer:false,
+    noAnswer:true};
+    
+var q6 = {
+    question:"The movie, Human Centipede, was based on a true story",
+    answer:false,
+    noAnswer:true};
+        
+var q7 = {
+    question:"You should never feed a Gremlin past midnight.",
+    answer:true,
+    noAnswer:true};
+        
+var q8 = {
+    question:"The ring was originally a Japanese horror film.",
+    answer:true,
+    noAnswer:true};
+    
+var q9 = {
+    question:"Jack the Ripper terrorized the city of Dublin, Ireland.",
+    answer:false,
+    noAnswer:true};
+            
+var q10 = {
+    question:"Reagan, from the movie the Exorcist, was possessed the demons in the magic 8 ball.",
+    answer:false,
+    noAnswer:true};
